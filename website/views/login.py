33690b4da984d1fd1f2b4f35220be0d864fc9f98
Lukas-Ldc <78859408+Lukas-Ldc@users.noreply.github.com>
2022-05-09 18:06:36 +0200

    Login page

--- website/views/login.py
+++ website/views/login.py
@@ -4,7 +4,17 @@ from django.contrib.auth import authenticate, login
 
 def login_view(request):
 
+    login_message = 0
+    if request.method == 'POST':
+        uAuth = authenticate(request, username=request.POST['user'], password=request.POST['pass'])
+        if uAuth is not None:
+             login(request, uAuth)
+             return redirect('website-accounts')
+        else:
+            login_message = 1
+
     context = {
         'page': 'login',
+        'log': login_message,
     }
     return render(request, "login.html", context)
