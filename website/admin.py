8e0ad7ced8d50d41726a7ce3a0e9117615807f9f
Lukas-Ldc <78859408+Lukas-Ldc@users.noreply.github.com>
2022-05-09 17:46:31 +0200

    Database

--- website/admin.py
+++ website/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin
+from .models import *
+
+class Account_Interface(admin.ModelAdmin):
+    list_display = ['unique', 'name', 'type', 'user', 'group', 'unit', 'comment']
+    search_fields = ['unqiue', 'name', 'type', 'user', 'group', 'unit', 'comment']
+
+class Transfer_Interface(admin.ModelAdmin):
+    list_display = ['source','destination','date','unit','amount','fee','feeType','comment']
+    search_fields = ['source','destination','date','unit','comment']
+
+class Transaction_Interface(admin.ModelAdmin):
+    list_display = ['account','market','type','date','input','output','amountIn','amountOut','multiplier','fee','feeType','comment']
+    search_fields = ['account','market','type','date','input','output','comment']
+
+admin.site.register(Account, Account_Interface)
+admin.site.register(Transfer, Transfer_Interface)
+admin.site.register(Transaction, Transaction_Interface)
--- website/models.py
+++ website/models.py
@@ -1,1 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Account(models.Model):
+    unique = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.CharField(max_length=50, null=True, blank=True)
+    unit = models.CharField(max_length=15, null=True, blank=True)
+    comment = models.CharField(max_length=255, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.unique = str(self.user) + '_' + str(self.name)
+        super().save(*args, **kwargs)
+
+class Transfer(models.Model):
+    source = models.ForeignKey(Account, to_field='unique', on_delete=models.CASCADE, related_name='source_account')
+    destination = models.ForeignKey(Account, to_field='unique', on_delete=models.CASCADE, related_name='destination_account')
+    date = models.DateTimeField(null=True, blank=True)
+    unit = models.CharField(max_length=15)
+    amount = models.DecimalField(max_digits=30, decimal_places=15)
+    fee = models.DecimalField(max_digits=30, decimal_places=15, null=True, blank=True)
+    feeType = models.CharField(max_length=15, null=True, blank=True)
+    comment = models.CharField(max_length=255, null=True, blank=True)
+
+class Transaction(models.Model):
+    account = models.ForeignKey(Account, to_field='unique', on_delete=models.CASCADE)
+    market = models.CharField(max_length=30, null=True, blank=True)
+    type = models.CharField(max_length=30, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    input = models.CharField(max_length=15)
+    output = models.CharField(max_length=15)
+    amountIn = models.DecimalField(max_digits=30, decimal_places=15)
+    amountOut = models.DecimalField(max_digits=30, decimal_places=15)
+    multiplier = models.DecimalField(max_digits=30, decimal_places=15)
+    fee = models.DecimalField(max_digits=30, decimal_places=15, null=True, blank=True)
+    feeType = models.CharField(max_length=15, null=True, blank=True)
+    comment = models.CharField(max_length=255, null=True, blank=True)
+
+Standard_Types = [
+    ('AccountType','AccountType'),
+    ('TransactionType','TransactionType'),
+]
+
+class Standard(models.Model):
+    type = models.CharField(max_length=30, choices=Standard_Types)
+    name = models.CharField(max_length=30)
