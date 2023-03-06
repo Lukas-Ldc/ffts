506b4f634eac9bdca76beda9f9f8d2caf3c16d87
Lukas-Ldc <78859408+Lukas-Ldc@users.noreply.github.com>
2023-03-06 22:04:22 +0100

    New account page backend

--- website/views/account.py
+++ website/views/account.py
@@ -1,4 +1,3 @@
-from decimal import DivisionByZero, InvalidOperation
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from website.models import Transfer, Transaction, Account
@@ -20,134 +19,143 @@ def account_view(request, name):
 
     # If the account belongs to the user who made the request
     if Account.objects.all().filter(user__exact=request.user, unique__exact=name).exists():
-        # FIXME: Account units total not always correct
 
         account = Account.objects.all().get(user__exact=request.user, unique__exact=name)
         transfers = Transfer.objects.all().filter(Q(source__exact=name) | Q(destination__exact=name))
-        transactions = Transaction.objects.all().filter(account__exact=name).order_by('input', 'output')
+        transactions = Transaction.objects.all().filter(account__exact=name)
         acc_units = str(account.unit).split(",")
+        all_units, volume_overview, pairs_summ, pairs_overview = [], [], [], []
 
-        # [data] :                (0) : UNIT
-        #       Transactions part (1) : ACC_UNIT | VOL_IN | VOL_OUT | VOL_ACC_UNIT_IN | VOL_ACC_UNIT_OUT | VOL_FEE | VOL_FEE_ACC_UNIT
-        #       Transfers part    (2) : VOL_IN | VOL_OUT | VOL_FEE_IN | VOL_FEE_OUT
-        data = []
-        overview = []
-        temp = []
-        acc = []
+        # ---- Assets Volume Calculations ----
 
-        # Data creation by finding all units
-        for unit in transfers.values('unit').order_by('unit').distinct():
-            data.append([str(unit)[:-2][10:], '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        # Getting all existing units linked to this account amoung transfers and transactions
+        for transfer in transfers.values('unit').order_by('unit').distinct():
+            all_units.append(transfer.get('unit'))
+        for transaction in transactions.values('input').order_by('input').distinct():
+            all_units.append(transaction.get('input'))
+        for transaction in transactions.values('output').order_by('output').distinct():
+            all_units.append(transaction.get('output'))
+        all_units = set(all_units)
 
-        for unit in transactions.values('input').order_by('input').distinct():
-            found = False
-            for ok_unit in data:
-                if str(unit)[:-2][11:] == ok_unit[0]:
-                    found = True
+        # Getting all the volume for each unit and the total amount in the account
+        for unit in all_units:
+            # Table : Total, Transaction In, Transaction Out, Transfer In, Transfer Out, Fee
+            table = [0, 0, 0, 0, 0, 0]
+
+            # Transfer In
+            for transfer in transfers.all().filter(destination__exact=account, unit__exact=unit):
+                table[0] = table[0] + transfer.amount
+                table[3] = table[3] + transfer.amount
+            # Transfer Out
+            for transfer in transfers.all().filter(source__exact=account, unit__exact=unit):
+                table[0] = table[0] - transfer.amount
+                table[4] = table[4] + transfer.amount
+            # Transactions In
+            for transaction in transactions.all().filter(input__exact=unit):
+                table[0] = table[0] - transaction.amountIn
+                table[1] = table[1] + transaction.amountIn
+            # Transactions Out
+            for transaction in transactions.all().filter(output__exact=unit):
+                table[0] = table[0] + transaction.amountOut
+                table[2] = table[2] + transaction.amountOut
+            # Transfer Fee
+            for transfer in transfers.all().filter(source__exact=account, feeUnit__exact=unit):
+                table[0] = table[0] - transfer.fee
+                table[5] = table[5] + transfer.fee
+            # Transaction Fee
+            for transaction in transactions.all().filter(feeUnit__exact=unit):
+                table[0] = table[0] - transaction.fee
+                table[5] = table[5] + transaction.fee
+            # Saving results
+            table = [unit] + [float_remover(float(t)) for t in table] + [1 if unit in acc_units else 0]
+            volume_overview.append(table)
+
+        # Sorting the results
+        volume_overview.sort(key=lambda x: x[0])  # Alphabetical order
+        volume_overview.sort(key=lambda x: 0 if x[1] != 0 else 1)  # Total Amount=0 at the end
+        volume_overview.sort(key=lambda x: 0 if x[0] in acc_units else 1)  # Account units first
+
+        # ---- Pairs Calculations ----
+
+        # Getting the totals amounts and fees
+        for transaction in transactions.all().order_by('date'):
+            table, index, pair = None, None, None
+
+            # Pair already saved
+            for saved_pair in pairs_summ:
+                if f"{transaction.input}/{transaction.output}" == saved_pair[0]:
+                    table = saved_pair
+                    index = pairs_summ.index(saved_pair)
                     break
-            if not found:
-                data.append([str(unit)[:-2][11:], '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-        for unit in transactions.values('output').order_by('output').distinct():
-            found = False
-            for ok_unit in data:
-                if str(unit)[:-2][12:] == ok_unit[0]:
-                    found = True
+                elif f"{transaction.output}/{transaction.input}" == saved_pair[0]:
+                    table = saved_pair
+                    index = pairs_summ.index(saved_pair)
                     break
-            if not found:
-                data.append([str(unit)[:-2][12:], '', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-        data = sorted(data, key=lambda x: x[0])
+            # New pair
+            if table is None:
+                # The account unit is on the right (better sort by name & calculations)
+                pair = f"{transaction.output}/{transaction.input}"
+                if transaction.output in acc_units:
+                    pair = f"{transaction.input}/{transaction.output}"
+                    if transaction.input in acc_units:
+                        if acc_units.index(transaction.input) < acc_units.index(transaction.output):
+                            pair = f"{transaction.output}/{transaction.input}"
 
-        # Adding Transactions data
-        for trans in transactions:
-            for unit in data:
+                # Pair ([1]/[2]), [1]Amount In, [2]Amount Out, [2]Amount In, [1]Amount Out, [1]Fees, [2]Fees
+                table = [pair, 0, 0, 0, 0, 0, 0]
 
-                if trans.input == unit[0]:
-                    unit[3] = unit[3] + trans.amountIn
-                    if trans.output in acc_units:
-                        if len(unit[1]) < 1:
-                            unit[1] = trans.output
-                            if unit[0] in acc_units:
-                                unit[1] = unit[0]
-                        if unit[1] == trans.output:
-                            unit[5] = unit[5] + trans.amountOut
-                    if trans.feeUnit == unit[1]:
-                        unit[7] = unit[7] + trans.fee
-
-                elif trans.output == unit[0]:
-                    unit[2] = unit[2] + trans.amountOut
-                    if trans.input in acc_units:
-                        if len(unit[1]) < 1:
-                            unit[1] = trans.input
-                            if unit[0] in acc_units:
-                                unit[1] = unit[0]
-                        if unit[1] == trans.input:
-                            unit[4] = unit[4] + trans.amountIn
-                    if trans.feeUnit == unit[1]:
-                        unit[7] = unit[7] + trans.fee
-
-                if trans.feeUnit == unit[0]:
-                    unit[6] = unit[6] + trans.fee
-
-        # Adding Transfers data
-        for trans in transfers:
-            for unit in data:
-
-                if trans.unit == unit[0]:
-                    if acc_clean(trans.source) == account.unique:
-                        unit[9] = unit[9] + trans.amount
-                    elif acc_clean(trans.destination) == account.unique:
-                        unit[8] = unit[8] + trans.amount
-
-                if trans.feeUnit == unit[0]:
-                    if acc_clean(trans.source) == account.unique:
-                        unit[11] = unit[11] + trans.fee
-                    elif acc_clean(trans.destination) == account.unique:
-                        unit[10] = unit[10] + trans.fee
-
-        # Assets Overview Calculs
-        for unit in data:
-            # if unit[2] > 0 or unit[3] > 0:
-
-            amount = (unit[2] - unit[3] - unit[6]) + (unit[8] - unit[9] - unit[11])
-            p_and_l = unit[5] - unit[4] - unit[7]
-
-            try:
-                avg_u_in = unit[4] / unit[2]
-            except (TypeError, ZeroDivisionError):
-                avg_u_in = 0
-
-            try:
-                avg_u_out = unit[5] / unit[3]
-            except (TypeError, ZeroDivisionError):
-                avg_u_out = 0
-
-            try:
-                perf = (unit[5] / unit[4]) * 100 - 100
-            except (TypeError, ZeroDivisionError, DivisionByZero, InvalidOperation):
-                perf = 0
-
-            if unit[0] in acc_units:
-                perf = avg_u_in = avg_u_out = 0
-                p_and_l = unit[2] - unit[3] - unit[6]
-
-            if unit[0] in acc_units:
-                acc.append([unit[0], unit[1], amount, perf, p_and_l, avg_u_in, avg_u_out, 1])
-            elif amount == 0:
-                temp.append([unit[0], unit[1], amount, perf, p_and_l, avg_u_in, avg_u_out, 0])
+            # Filling the table
+            if transaction.input == table[0].split("/")[0]:
+                table[1] = table[1] + float(transaction.amountIn)
+                table[2] = table[2] + float(transaction.amountOut)
             else:
-                overview.append([unit[0], unit[1], amount, perf, p_and_l, avg_u_in, avg_u_out, 0])
+                table[3] = table[3] + float(transaction.amountIn)
+                table[4] = table[4] + float(transaction.amountOut)
 
-        overview = acc + overview + temp
+            if transaction.feeUnit == table[0].split("/")[0]:
+                table[5] = table[5] + float(transaction.fee)
+            elif transaction.feeUnit == table[0].split("/")[1]:
+                table[6] = table[6] + float(transaction.fee)
+
+            # Saving the results
+            if index is not None:
+                pairs_summ[index] = table
+            else:
+                pairs_summ.append(table)
+
+        # Calculating the numbers
+        for pair in pairs_summ:
+            # Account unit on the right, so the left element is the one we 'buy' and 'sell'
+            try:
+                avg_buy = float_remover(pair[3] / pair[4])
+            except ZeroDivisionError:
+                avg_buy = 0
+            try:
+                avg_sell = float_remover(pair[2] / pair[1])
+            except ZeroDivisionError:
+                avg_sell = 0
+            pnl = float_remover(pair[2] - pair[3])
+            try:
+                perf = round(((pair[2] / pair[3]) - 1) * 100, 2)
+            except ZeroDivisionError:
+                perf = 0
+            fee = float_remover(pair[6])
+
+            pairs_overview.append([pair[0], perf, pnl, avg_buy, avg_sell, fee])
+
+        # Sorting the results
+        pairs_overview.sort(key=lambda x: x[0])
+
+        # ---- End ----
 
         # Web page rendering
         context = {
             'file': 'account',
             'title': f"Account - {account.name}",
             'account': account,
-            'data': data,
-            'overview': overview,
+            'volume_overview': volume_overview,
+            'pairs_overview': pairs_overview
         }
         return render(request, "account.html", context)
 
@@ -155,14 +163,21 @@ def account_view(request, name):
     return redirect('website-accounts')
 
 
-def acc_clean(account: str):
-    """Cleans an account object name:
-    "Account object (acc_name)" -> "acc_name"
+def float_remover(number: float):
+    """Depending on the size of the number, limits the number of digits after the decimal point
 
     Args:
-        account (str): The account object name
+        number (float): The number to clean
 
     Returns:
-        str: The account name
+        float: The cleaned number
     """
-    return str(account)[:-1].replace("Account object (", "")
+    if number > 1000:
+        return round(number, 1)
+    if number > 100:
+        return round(number, 2)
+    if number > 10:
+        return round(number, 2)
+    if number > 1:
+        return round(number, 4)
+    return round(number, 8)
